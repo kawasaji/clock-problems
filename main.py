e237badb7cbfc592103e497865c5bec78a45565d
kawasaji <jimmyhuseynli@gmail.com>
2023-01-08 15:21:29 +0400

    added color

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import math
 import os
+from colorama import init, Fore
+from colorama import Back
+from colorama import Style
 
 def type_one(hour, minute):
     number = abs(11 * minute - 60 * hour) / 2
     if number > 180:
-        number = 360 - number
-    return number
+        return 360 - number
+    else:
+        return number
 
 os.system("cls")
 
 while True:
 
-    hour = int(input("enter hour ~# "))
-    minute = int(input("enter minute ~# "))
+    hour = int(input(Fore.BLUE + "enter hour ~# " + Fore.GREEN))
+    minute = int(input(Fore.BLUE + "enter minute ~# " + Fore.GREEN))
     number = type_one(hour, minute)
-    print(number)
+    print(Fore.MAGENTA + str(number))
