397d6c9cb48b2314809d7e57c958b263c4cbc094
kawasaji <jimmyhuseynli@gmail.com>
2023-01-08 14:57:54 +0400

    added main.py and finished project

---
+++ main.py
@@ -0,0 +1,13 @@
+import math
+
+def type_one(hour, minute):
+    number = abs(11 * minute - 60 * hour) / 2
+    if number > 180:
+        number = 360 - number
+    return number
+
+while True:
+    hour = int(input("enter hour ~# "))
+    minute = int(input("enter minute ~# "))
+    number = type_one(hour, minute)
+    print(number)
